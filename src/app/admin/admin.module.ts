7df0369f18689e8aa0a46033ad206d7e69884ad8
AMZURTECH\SudheerM <sudheer.meesala@amzurtech.com>
2017-11-29 19:47:09 +0530

    added carrierRule config screen

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ReactiveFormsModule,FormsModule} from '@angular/forms';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { RuleListComponent } from './rule-list/rule-list.component';
 import {DataTableModule,SharedModule} from 'primeng/primeng';
 import {PaginatorModule} from 'primeng/primeng';
- 
+import {DropdownModule} from 'primeng/primeng';
 
  
 import {ButtonModule} from 'primeng/primeng';
@@ -14,8 +15,8 @@ import {DialogModule} from 'primeng/primeng';
 import { CarrierRuleConfigComponent } from './carrier-rule-config/carrier-rule-config.component'; 
 @NgModule({
   imports: [
-    CommonModule,
-    AdminRoutingModule,PaginatorModule,DataTableModule,SharedModule,ButtonModule,InputTextModule,DialogModule
+    CommonModule,FormsModule,ReactiveFormsModule,
+    AdminRoutingModule,PaginatorModule,DataTableModule,SharedModule,ButtonModule,InputTextModule,DialogModule,DropdownModule
   ],
   declarations: [RuleListComponent, CarrierRuleConfigComponent],
   exports:[RuleListComponent]
--- src/app/admin/carrier-rule-config/carrier-rule-config.component.ts
+++ src/app/admin/carrier-rule-config/carrier-rule-config.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormControl,FormArray,FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-carrier-rule-config',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrierRuleConfigComponent implements OnInit {
 
-  constructor() { }
+ // constructor() { }
+ CRCform:FormGroup;
 
   ngOnInit() {
+    this.createForm();
   }
+
+      cities: any[];
+  
+      selectedCity: string;
+  
+      cars: any[];
+  
+      selectedCar: string;
+  
+      selectedCar2: string = 'BMW';
+  
+      constructor(private fb:FormBuilder) {
+          this.cities = [
+              {name: 'New York', code: 'NY'},
+              {name: 'Rome', code: 'RM'},
+              {name: 'London', code: 'LDN'},
+              {name: 'Istanbul', code: 'IST'},
+              {name: 'Paris', code: 'PRS'}
+          ];
+  
+          this.cars = [
+              {label: 'Audi', value: 'Audi'},
+              {label: 'BMW', value: 'BMW'},
+              {label: 'Fiat', value: 'Fiat'},
+              {label: 'Ford', value: 'Ford'},
+              {label: 'Honda', value: 'Honda'},
+              {label: 'Jaguar', value: 'Jaguar'},
+              {label: 'Mercedes', value: 'Mercedes'},
+              {label: 'Renault', value: 'Renault'},
+              {label: 'VW', value: 'VW'}
+              
+          ];
+      }
+      createForm() {
+        this.CRCform = this.fb.group({
+          mcc: 'BMW',  
+          carrierGp:'',
+          commonCG:''
+        });
+      }
 
 }
