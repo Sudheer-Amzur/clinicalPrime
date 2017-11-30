0cb7249e3fe87d1cdc0bc98ffa5f011981a71d8f
AMZURTECH\SudheerM <sudheer.meesala@amzurtech.com>
2017-11-30 12:20:36 +0530

    added multi select grid

--- src/app/admin/carrier-rule-config/carrier-rule-config.component.ts
+++ src/app/admin/carrier-rule-config/carrier-rule-config.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder,FormControl,FormArray,FormGroup} from '@angular/forms';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-carrier-rule-config',
@@ -24,28 +25,36 @@ export class CarrierRuleConfigComponent implements OnInit {
       selectedCar: string;
   
       selectedCar2: string = 'BMW';
+
+      gridData:any[];
   
       constructor(private fb:FormBuilder) {
-          this.cities = [
-              {name: 'New York', code: 'NY'},
-              {name: 'Rome', code: 'RM'},
-              {name: 'London', code: 'LDN'},
-              {name: 'Istanbul', code: 'IST'},
-              {name: 'Paris', code: 'PRS'}
-          ];
+        this.cities = [{name: 'New York', code: 'NY'},
+                       {name: 'Rome', code: 'RM'},
+                       {name: 'London', code: 'LDN'},
+                       {name: 'Istanbul', code: 'IST'},
+                       {name: 'Paris', code: 'PRS'}];
+        this.cars = [{label: 'Audi', value: 'Audi'},
+                     {label: 'BMW', value: 'BMW'},
+                     {label: 'Fiat', value: 'Fiat'},
+                     {label: 'Ford', value: 'Ford'},
+                     {label: 'Honda', value: 'Honda'},
+                     {label: 'Jaguar', value: 'Jaguar'},
+                     {label: 'Mercedes', value: 'Mercedes'},
+                     {label: 'Renault', value: 'Renault'},
+                     {label: 'VW', value: 'VW'}];
+        this.gridData=[
+                    {"brand": "VW", "year": 2012, "color": "Orange", "vin": "dsad231ff"},
+                    {"brand": "Audi", "year": 2011, "color": "Black", "vin": "gwregre345"},
+                    {"brand": "Renault", "year": 2005, "color": "Gray", "vin": "h354htr"},
+                    {"brand": "BMW", "year": 2003, "color": "Blue", "vin": "j6w54qgh"},
+                    {"brand": "Mercedes", "year": 1995, "color": "Orange", "vin": "hrtwy34"},
+                    {"brand": "Volvo", "year": 2005, "color": "Black", "vin": "jejtyj"},
+                    {"brand": "Honda", "year": 2012, "color": "Yellow", "vin": "g43gr"},
+                    {"brand": "Jaguar", "year": 2013, "color": "Orange", "vin": "greg34"},
+                    {"brand": "Ford", "year": 2000, "color": "Black", "vin": "h54hw5"},
+                    {"brand": "Fiat", "year": 2013, "color": "Red", "vin": "245t2s"}]
   
-          this.cars = [
-              {label: 'Audi', value: 'Audi'},
-              {label: 'BMW', value: 'BMW'},
-              {label: 'Fiat', value: 'Fiat'},
-              {label: 'Ford', value: 'Ford'},
-              {label: 'Honda', value: 'Honda'},
-              {label: 'Jaguar', value: 'Jaguar'},
-              {label: 'Mercedes', value: 'Mercedes'},
-              {label: 'Renault', value: 'Renault'},
-              {label: 'VW', value: 'VW'}
-              
-          ];
       }
       createForm() {
         this.CRCform = this.fb.group({
@@ -54,5 +63,13 @@ export class CarrierRuleConfigComponent implements OnInit {
           commonCG:''
         });
       }
+      onRowSelect(event) {
+        console.log(event.data.vin);
+         
+    }
+
+    onRowUnselect(event) {
+        
+    }
 
 }
