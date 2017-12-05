0eb1efe0958abd7c994f2796d8783f049743eabd
AMZURTECH\NarendraK <narendra.kundeti@amzurtech.com>
2017-12-05 13:40:21 +0530

    Added Create-Rule Page

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,18 +7,20 @@ import { RuleListComponent } from './rule-list/rule-list.component';
 import {DataTableModule,SharedModule} from 'primeng/primeng';
 import {PaginatorModule} from 'primeng/primeng';
 import {DropdownModule} from 'primeng/primeng';
-import {StepsModule,MenuItem} from 'primeng/primeng';
-
- 
+import {StepsModule,MenuItem,AccordionModule} from 'primeng/primeng';
+import {CalendarModule} from 'primeng/primeng';
+import {InputSwitchModule} from 'primeng/primeng';
 import {ButtonModule} from 'primeng/primeng';
 import {InputTextModule} from 'primeng/primeng';
 import {DialogModule} from 'primeng/primeng';
 import { CarrierRuleConfigComponent } from './carrier-rule-config/carrier-rule-config.component';
 import { CreateRuleComponent } from './create-rule/create-rule.component'; 
+
 @NgModule({
   imports: [
     CommonModule,FormsModule,ReactiveFormsModule,
-    AdminRoutingModule,PaginatorModule,DataTableModule,SharedModule,ButtonModule,InputTextModule,DialogModule,DropdownModule,StepsModule
+    AdminRoutingModule,PaginatorModule,DataTableModule,SharedModule,ButtonModule,InputTextModule,
+    DialogModule,DropdownModule,StepsModule,AccordionModule,CalendarModule,InputSwitchModule
   ],
   declarations: [RuleListComponent, CarrierRuleConfigComponent, CreateRuleComponent],
   exports:[RuleListComponent]
