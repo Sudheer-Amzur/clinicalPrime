551be934f087ce33bf72ef25daaec7b31b17c4b3
AMZURTECH\SudheerM <sudheer.meesala@amzurtech.com>
2017-12-05 17:33:31 +0530

    added grid data

--- src/app/admin/carrier-rule-config/carrier-rule-config.component.ts
+++ src/app/admin/carrier-rule-config/carrier-rule-config.component.ts
@@ -49,6 +49,17 @@ export class CarrierRuleConfigComponent implements OnInit {
               {label: 'VW', value: 'VW'}
               
           ];
+          this.gridData=[
+            {brand: "VW", year: 2012, color: "Orange", vin: "dsad231ff",cityData:[]},
+            {brand: "Audi", year: 2011, color: "Black", vin: "gwregre345",cityData:[]},
+            {brand: "Renault", year: 2005, color: "Gray", vin: "h354htr",cityData:[]},
+            {brand: "BMW", year: 2003, color: "Blue", vin: "j6w54qgh",cityData:[]},
+            {brand: "Mercedes", year: 1995, color: "Orange", vin: "hrtwy34",cityData:[]},
+            {brand: "Volvo", year: 2005, color: "Black", vin: "jejtyj",cityData:[]},
+            {brand: "Honda", year: 2012, color: "Yellow", vin: "g43gr",cityData:[]},
+            {brand: "Jaguar",year: 2013, color: "Orange", vin: "greg34",cityData:[]},
+            {brand: "Ford",year: 2000, color: "Black", vin: "h54hw5",cityData:[]},
+            {brand: "Fiat", year: 2013, color: "Red", vin: "245t2s",cityData:[]}];
           this.message.changeMessage("Carrier-Rule-Config");
       }
       ngOnInit() {
